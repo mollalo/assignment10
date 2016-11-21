ca720f0f33ee4dd3f7e918fb6de7e7d78474b128
AbolfazlM <Abolfazl@ufl.edu>
2016-11-21 03:43:05 -0500

    Calculates GC_content for a vector of sequence

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -11,3 +11,33 @@ houseelf_data <- function() {
 data <- houseelf_data()
 
 
+#Question5
+install.packages("stringr")
+library(stringr)
+sequence <- "attggc"
+get_gc_content <- function(sequence) {
+  Gs <- str_count(sequence, "g")
+  Cs <- str_count(sequence, "c")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100 
+  return(gc_content)
+}
+get_gc_content (sequence)
+
+sequences <- c("ttgaatgccttacaactgatcattacacaggcggcatgaagcaaaaatatactgtgaaccaatgcaggcg",
+               "gauuauuccccacaaagggagugggauuaggagcugcaucauuuacaagagcagaauguuucaaaugcau",
+               "gaaagcaagaaaaggcaggcgaggaagggaagaagggggggaaacc",
+               "guuuccuacaguauuugaugagaaugagaguuuacuccuggaagauaauauuagaauguuuacaacugcaccugaucagguggauaaggaagaugaagacu",
+               "gauaaggaagaugaagacuuucaggaaucuaauaaaaugcacuccaugaauggauucauguaugggaaucagccggguc")
+
+empty <- c ()
+for (seq in sequences){
+  gc_content <- c(empty, get_gc_content (seq))
+  print(gc_content)
+}
+
+
+
+
+
+
+
