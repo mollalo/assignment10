fbde9d8cb1e6ebc79c9c08bfa39971d360fd0833
Abolfazl Mollalo <abolfazl@ufl.edu>
2016-11-20 22:11:15 -0500

    Import houseelf-earlength-dna-data.csv into R

---
+++ houseelf-analysis.R
@@ -0,0 +1,11 @@
+#add a comment at the top describing what the analysis is intended to do.
+#Commit this file to version control with a good commit message.
+#Then check to see if you can see this commit in the history.
+#
+
+
+###Q3
+#Add some code to houseelf-analysis.R that imports the data into R.
+#Commit these changes to version control
+
+data_houseelf <- read.csv("./data/houseelf-earlength-dna-data.csv")
