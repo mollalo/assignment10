345d416f83d8af7074445d63ee133d0341bc1c9c
Abolfazl Mollalo <abolfazl@ufl.edu>
2016-11-21 01:43:11 -0500

    get data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -7,6 +7,10 @@
 ###Q3
 #Add some code to houseelf-analysis.R that imports the data into R.
 #Commit these changes to version control
+get_data <- function() {
+  data <- read.csv("./data/houseelf_earlength_dna_data_1.csv")
+  return(data)
+}
 
-  data_houseelf <- read.csv("./data/houseelf_earlength_dna_data_1.csv")
+data <- get_data()
 
